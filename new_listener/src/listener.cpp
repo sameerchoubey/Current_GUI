232df7e5b99486e1852b40467a7fa48b4393ac1a
vernwalrahul <vernwalrahul@gmail.com>
2017-09-29 13:46:45 +0530

    currently working packages

---
+++ new_listener/src/listener.cpp
@@ -0,0 +1,94 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "krssg_ssl_msgs/BeliefState.h"
+#include "krssg_ssl_msgs/planner_path.h"
+#include "krssg_ssl_msgs/point_2d.h"
+#include <iostream>
+#include "MotionPlanner.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace cv;
+
+std::vector<point> path_points;
+std::vector<pos> v;
+krssg_ssl_msgs::planner_path points;
+krssg_ssl_msgs::point_2d point_;
+ros::Publisher pub;
+//Planning *planning;
+
+void Callback(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
+{
+  ROS_INFO("SHUBHAM,I AM IN CALLBACK!");
+
+  pos p;
+  v.clear();
+  for(int i=0;i<msg->homePos.size();i++){
+  	p.x=fabs(msg->homePos[i].x+3300)/8.0;
+  	p.y=fabs(msg->homePos[i].y+2200)/8.0;
+  	//cout<<p.x<<" "<<p.y<<endl;
+  	v.push_back(p);
+  }
+
+  for(int i=0;i<msg->awayPos.size();i++){
+  	p.x=fabs(msg->awayPos[i].x+3300)/8.0;
+  	p.y=fabs(msg->awayPos[i].y+2200)/8.0;
+  	v.push_back(p);
+  }
+
+  Planning planning(v,v.size());
+  // planning.planWithSimpleSetup();
+  // planning.drawPath();
+  // planning.output();
+
+  planning.planSimple();
+  planning.plan(v[0].x, v[0].y, 800, 800);
+  path_points = planning.recordSolution();
+  planning.drw();
+   for(int i=0;i<path_points.size();i++)
+    {
+      point_.x=path_points[i].x;
+      point_.y=path_points[i].y;
+      points.point_array.push_back(point_);
+      // cout<<path[i].x<<","<<path[i].y<<endl;
+    }   
+
+   
+
+  pub.publish(points); cout<<"data published "<<endl;
+}
+
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "listener");
+
+  ros::NodeHandle n;
+
+  cout<<"HEY SHUBHAM!"<<endl;
+
+  /*pos p;
+  time_t sec;
+  time(&sec);
+  srand((unsigned int)sec);*/
+
+  ros::Subscriber sub = n.subscribe("/belief_state", 1000, Callback);
+  pub = n.advertise<krssg_ssl_msgs::planner_path>("path_planner", 1000);
+  /*for(int i=0;i<12;i++){
+  	p.x=400.0;//rand()%800;
+  	p.y=400.0;//rand()%800;
+  	v.push_back(p);
+  }
+
+  while(1){
+  Planning planning(v,v.size());
+  planning.planWithSimpleSetup();
+  planning.drawPath();
+  planning.output();
+  }*/
+
+  ros::spin();
+
+  return 0;
+}
